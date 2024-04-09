cadd526f85239afc7bd0cc9ed9952697499025a2
Jeanette Müller <themaverick@themaverick.de>
2024-04-09 20:27:47 +0200

    cleanup

--- src/wifi/Wifi.h
+++ src/wifi/Wifi.h
@@ -10,55 +10,53 @@ bool isConnected = false;
 
 void setupWifi()
 {
-  if (WiFiMode == 0)
-  {
-    WiFi.mode(WIFI_AP);
-    WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
-    WiFi.softAP(WifiSsid, WifiPassword, 1, false);
-  }
-  else
-  {
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(WifiSsid, WifiPassword);
-  }
+    if (WiFiMode == 0)
+    {
+        WiFi.mode(WIFI_AP);
+        WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+        WiFi.softAP(WifiSsid, WifiPassword, 1, false);
+    }
+    else
+    {
+        WiFi.mode(WIFI_STA);
+        WiFi.begin(WifiSsid, WifiPassword);
+    }
 }
 
 void loopWifi()
 {
-
-  if (WiFiMode == 0)
-  {
-    if (WiFi.status() == WL_CONNECTED)
+    if (WiFiMode == 0)
     {
-
-      if (!isConnected)
-      {
-        isConnected == true;
-        Serial.println(WiFi.localIP());
-      }
-    }
-  }
-  else
-  {
-    if (WiFi.status() == WL_CONNECTED)
-    {
-      // Serial.println(WiFi.localIP());
+        if (WiFi.status() == WL_CONNECTED)
+        {
+            if (!isConnected)
+            {
+                isConnected = true;
+                Serial.println(WiFi.localIP());
+            }
+        }
     }
     else
     {
-      int intRetry = 0;
-      Serial.print("Connecting to WiFi ..");
-      while (WiFi.status() != WL_CONNECTED && (intRetry < 50))
-      {
-        Serial.print('.');
-        intRetry++;
-        delay(500);
-      }
+        if (WiFi.status() == WL_CONNECTED)
+        {
+            // Serial.println(WiFi.localIP());
+        }
+        else
+        {
+            int intRetry = 0;
+            Serial.print("Connecting to WiFi ..");
+            while (WiFi.status() != WL_CONNECTED && (intRetry < 50))
+            {
+                Serial.print('.');
+                intRetry++;
+                delay(500);
+            }
 
-      if (WiFi.status() == WL_CONNECTED)
-      {
-        Serial.println(WiFi.localIP());
-      }
+            if (WiFi.status() == WL_CONNECTED)
+            {
+                Serial.println(WiFi.localIP());
+            }
+        }
     }
-  }
 }
