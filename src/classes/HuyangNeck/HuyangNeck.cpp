6a2ba9a0bf45aae2a82094b3921fef946f012f54
Jeanette Müller <themaverick@themaverick.de>
2024-04-15 21:59:17 +0200

    only animate if target is not what i need

--- src/classes/HuyangNeck/HuyangNeck.cpp
+++ src/classes/HuyangNeck/HuyangNeck.cpp
@@ -90,7 +90,7 @@ double HuyangNeck::easeInAndOut(double start, double current, double target, dou
 void HuyangNeck::updateNeckPosition()
 {
 	if (_currentTiltForward != targetTiltForward)
-	{	
+	{
 		double tiltForwardPercentage = (_currentMillis - _tiltForwardStartMillis) / _tiltForwardDuration;
 		_currentTiltForward = easeInAndOut(_startTiltForward, _currentTiltForward, targetTiltForward, tiltForwardPercentage);
 	}
@@ -118,57 +118,69 @@ void HuyangNeck::tiltSideways(double degree)
 	degree = min(degree, _maxTiltSideways);
 	degree = max(degree, _minTiltSideways);
 
-	_startTiltSideways = _currentTiltSideways;
-	targetTiltSideways = degree;
-	_tiltSidewaysPercentage = 0.0;
+	if (targetTiltSideways != degree)
+	{
+		_startTiltSideways = _currentTiltSideways;
+		targetTiltSideways = degree;
+		_tiltSidewaysPercentage = 0.0;
+	}
 }
 void HuyangNeck::tiltForward(double degree, double duration)
 {
 	degree = min(degree, _maxTiltForward);
 	degree = max(degree, _minTiltForward);
 
-	if (duration == 0)
-	{	
-		double way = 0;
-		if (degree > _currentTiltForward) {
-			way = degree - _currentTiltForward;
-		}
-		else
+	if (targetTiltForward != degree)
+	{
+		if (duration == 0)
 		{
-			way = _currentTiltForward - degree;
+			double way = 0;
+			if (degree > _currentTiltForward)
+			{
+				way = degree - _currentTiltForward;
+			}
+			else
+			{
+				way = _currentTiltForward - degree;
+			}
+
+			duration = way * 16;
 		}
 
-		duration = way * 16; 
+		_startTiltForward = _currentTiltForward;
+		targetTiltForward = degree;
+		_tiltForwardDuration = duration;
+		_tiltForwardStartMillis = _currentMillis;
 	}
-
-	_startTiltForward = _currentTiltForward;
-	targetTiltForward = degree;
-	_tiltForwardDuration = duration;
-	_tiltForwardStartMillis = _currentMillis;
 }
 void HuyangNeck::rotate(double degree, double duration)
 {
 	degree = min(degree, _maxRotation);
 	degree = max(degree, _minRotation);
 
-	if (duration == 0)
-	{	
-		double way = 0;
-		if (degree > _currentRotate) {
-			way = degree - _currentRotate;
-		}
-		else
+	if (targetRotate != degree)
+	{
+
+		if (duration == 0)
 		{
-			way = _currentRotate - degree;
+			double way = 0;
+			if (degree > _currentRotate)
+			{
+				way = degree - _currentRotate;
+			}
+			else
+			{
+				way = _currentRotate - degree;
+			}
+
+			duration = way * 16;
 		}
 
-		duration = way * 16; 
+		_startRotate = _currentRotate;
+		targetRotate = degree;
+		_rotationDuration = duration;
+		_rotationStartMillis = _currentMillis;
 	}
-
-	_startRotate = _currentRotate;
-	targetRotate = degree;
-	_rotationDuration = duration;
-	_rotationStartMillis = _currentMillis;
 }
 
 void HuyangNeck::updateCurrentRotate()
@@ -177,7 +189,7 @@ void HuyangNeck::updateCurrentRotate()
 	{
 		double percentage = (_currentMillis - _rotationStartMillis) / _rotationDuration;
 		_currentRotate = easeInAndOut(_startRotate, _currentRotate, targetRotate, percentage);
-		
+
 		uint16_t rotateDegree = map(_currentRotate, _minRotation, _maxRotation, 0, 110);
 		rotateServo(pwm_pin_head_rotate, rotateDegree);
 	}
@@ -189,46 +201,46 @@ void HuyangNeck::setup()
 	tiltForward(0);
 }
 
-void HuyangNeck::doRandomRotate() 
+void HuyangNeck::doRandomRotate()
 {
 	if (_randomDoRotate == 0)
-		{
-			_randomDoRotate = _currentMillis + 2000 + (random(6, 12 + 1) * 1000);
-		}
+	{
+		_randomDoRotate = _currentMillis + 2000 + (random(6, 12 + 1) * 1000);
+	}
 
-		if (_currentMillis > _randomDoRotate)
-		{
-			_randomDoRotate = 0;
+	if (_currentMillis > _randomDoRotate)
+	{
+		_randomDoRotate = 0;
 
-			if (_currentRotate > 0)
-			{
-				rotate(-(random(2, 35)), random(2, 6 + 1) * 1000);
-			}
-			else
-			{
-				rotate(random(2, 35), random(2, 6 + 1) * 1000);
-			}
+		if (_currentRotate > 0)
+		{
+			rotate(-(random(2, 35)), random(2, 6 + 1) * 1000);
 		}
+		else
+		{
+			rotate(random(2, 35), random(2, 6 + 1) * 1000);
+		}
+	}
 }
 
-void HuyangNeck::doRandomTiltForward() 
+void HuyangNeck::doRandomTiltForward()
 {
 	if (_randomDoTiltForward == 0)
-		{
-			_randomDoTiltForward = _currentMillis + 2500 + (random(6, 12 + 1) * 1050);
-		}
+	{
+		_randomDoTiltForward = _currentMillis + 2500 + (random(6, 12 + 1) * 1050);
+	}
 
-		if (_currentMillis > _randomDoTiltForward)
-		{
-			_randomDoTiltForward = 0;
+	if (_currentMillis > _randomDoTiltForward)
+	{
+		_randomDoTiltForward = 0;
 
-			double workArea = _maxTiltForward - _minTiltForward;
-			double center = workArea / 2;
+		double workArea = _maxTiltForward - _minTiltForward;
+		double center = workArea / 2;
 
-			double randomArea = workArea / 3;
+		double randomArea = workArea / 3;
 
-			tiltForward(random(center-randomArea, center+randomArea+1), random(3, 6 + 1) * 1000);
-		}
+		tiltForward(random(center - randomArea, center + randomArea + 1), random(3, 6 + 1) * 1000);
+	}
 }
 
 void HuyangNeck::loop()
