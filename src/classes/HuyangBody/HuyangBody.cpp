c1ecaa1f689a49f0ad5c2e3d3d6267913615ed66
Jeanette Müller <themaverick@themaverick.de>
2024-06-30 22:12:44 +0200

    chest lights activated

--- src/classes/HuyangBody/HuyangBody.cpp
+++ src/classes/HuyangBody/HuyangBody.cpp
@@ -1,23 +1,29 @@
 #include "HuyangBody.h"
 #include <Adafruit_PWMServoDriver.h>
 
-//Servo Parameters
-#define HuyangBody_SERVOMIN  150 // This is the 'minimum' pulse length count (out of 4096)
-#define HuyangBody_SERVOMAX  595 // This is the 'maximum' pulse length count (out of 4096)
+// Servo Parameters
+#define HuyangBody_SERVOMIN 150	 // This is the 'minimum' pulse length count (out of 4096)
+#define HuyangBody_SERVOMAX 595	 // This is the 'maximum' pulse length count (out of 4096)
 #define HuyangBody_SERVO_FREQ 60 // Analog servos run at ~50 Hz updates
 
-#define pwm_pin_sideway_left     (uint8_t)    14
-#define pwm_pin_sideway_right    (uint8_t)    15
-#define pwm_pin_forward_left     (uint8_t)    12
-#define pwm_pin_forward_right    (uint8_t)    13
-#define pwm_pin_body_rotate      (uint8_t)    11
+#define pwm_pin_sideway_left (uint8_t)14
+#define pwm_pin_sideway_right (uint8_t)15
+#define pwm_pin_forward_left (uint8_t)12
+#define pwm_pin_forward_right (uint8_t)13
+#define pwm_pin_body_rotate (uint8_t)11
 
-HuyangBody::HuyangBody(Adafruit_PWMServoDriver *pwm) {
+HuyangBody::HuyangBody(Adafruit_PWMServoDriver *pwm)
+{
 	_pwm = pwm;
+
+	_neoPixelLights = new Adafruit_NeoPixel(2, 0, pixelFormat);
+	_neoPixelLights->setBrightness(20);
+	_neoPixelLights->begin();
 }
 
-void HuyangBody::rotateServo(uint8_t servo, uint16_t degree) {
-	uint16_t pulselength = map(degree, 0, 180, HuyangBody_SERVOMIN, HuyangBody_SERVOMAX);      //  Calibrate the positive range (see below)
+void HuyangBody::rotateServo(uint8_t servo, uint16_t degree)
+{
+	uint16_t pulselength = map(degree, 0, 180, HuyangBody_SERVOMIN, HuyangBody_SERVOMAX); //  Calibrate the positive range (see below)
 	/*
 	Serial.print("rotateServoToDegree Servo: ");
 	Serial.print(servo);
@@ -30,37 +36,55 @@ void HuyangBody::rotateServo(uint8_t servo, uint16_t degree) {
 	// Serial.println("done");
 }
 
-void HuyangBody::tiltSideways(uint16_t degree) {
+void HuyangBody::tiltSideways(uint16_t degree)
+{
 	rotateServo(pwm_pin_sideway_left, _centerTiltSideways + degree);
 	rotateServo(pwm_pin_sideway_right, _centerTiltSideways + degree);
 }
-void HuyangBody::tiltForward(uint16_t degree){
+void HuyangBody::tiltForward(uint16_t degree)
+{
 	rotateServo(pwm_pin_forward_left, _centerTiltFront_left + degree);
 	rotateServo(pwm_pin_forward_right, _centerTiltFront_right - degree);
 }
-void HuyangBody::rotate(uint16_t degree) {
-	//center = 35
-	uint16_t rotateDegree  = map(degree, -100, 100, 0, 70);
+void HuyangBody::rotate(uint16_t degree)
+{
+	// center = 35
+	uint16_t rotateDegree = map(degree, -100, 100, 0, 70);
 	rotateServo(pwm_pin_body_rotate, rotateDegree);
 }
 
-void HuyangBody::centerAll() {
+void HuyangBody::centerAll()
+{
 	tiltSideways(0);
 	tiltForward(0);
 	rotate(0);
 }
 
-void HuyangBody::startCheckstLights() {
-	
+void HuyangBody::updateChestLights()
+{
+	uint32_t colorDefaultBlue = _neoPixelLights->Color(0, 0, 128);
+
+	_neoPixelLights->setPixelColor(0, colorDefaultBlue);
+	_neoPixelLights->setPixelColor(1, colorDefaultBlue);
+
+	_neoPixelLights->show();
 }
 
 void HuyangBody::setup()
 {
-	startCheckstLights();
 }
 
 void HuyangBody::loop()
 {
 	_currentMillis = millis();
 
+	if (_previousMillis > _currentMillis)
+	{
+		_previousMillis = _currentMillis;
+	}
+
+	if (_currentMillis - _previousMillis > 100)
+	{
+		updateChestLights();
+	}
 }
--- src/classes/HuyangBody/HuyangBody.h
+++ src/classes/HuyangBody/HuyangBody.h
@@ -3,6 +3,7 @@
 
 #include "Arduino.h"
 #include <Adafruit_PWMServoDriver.h>
+#include <Adafruit_NeoPixel.h>
 
 class HuyangBody
 {
@@ -19,7 +20,7 @@ public:
 
     void centerAll();
 
-    void startCheckstLights();
+    void updateChestLights();
 
 private:
     Adafruit_PWMServoDriver *_pwm;
@@ -32,6 +33,9 @@ private:
     int16_t _centerTiltFront_right = 100;
 
     void rotateServo(uint8_t servo, uint16_t degree);
+
+    Adafruit_NeoPixel *_neoPixelLights;
+    neoPixelType pixelFormat = NEO_GRB + NEO_KHZ800;
 };
 
 #endif
